0100db1c54a4fa87046c7f5d6a4dda851320248f
artemiygusev04 <PMPO1002@yandex.ru>
2023-01-22 20:18:57 +0300

    refactor 1

--- base_page/interface_map.py
+++
@@ -1,9 +0,0 @@
-from selene.support.shared.jquery_style import s
-
-
-def object_creation_mode(type_object):
-    s('.map-layers').s('svg[fill="white"]').click()
-    s(f'(//*[@class="base-menu layer-item__header-actions-btn"])[{str(type_object)}]').click()
-    s('.base-menu-item').hover()
-    s('//*[@class="base-menu-item__name"][text()="Редактировать"]').hover()
-    s('//*[@class="base-menu-item__name"][text()="Создать объект"]').click()
