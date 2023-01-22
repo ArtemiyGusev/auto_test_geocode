407850a56cf848dedb112faba00b03a0b82db07e
artemiygusev04 <PMPO1002@yandex.ru>
2023-01-22 20:06:23 +0300

    add repository

---
+++ auto_test_geocode/base_page/login.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from selene.support.shared.jquery_style import s
+from controls.helper.ui_tests_helper import url_open_size
+
+
+def open_page_and_login(url='/', width=1920, height=1080):
+    url_open_size(url, width, height)
+    load_dotenv()
+
+    def login(email=os.getenv('EMAIL_GEOCODE'), password=os.getenv('PASSWORD_GEOCODE')):
+
+        form_login = s('#kc-form-login')
+
+        form_login.s('#username').type(email)
+        form_login.s('#password').type(password)
+        form_login.s('#kc-login').click()
+
+    login()
